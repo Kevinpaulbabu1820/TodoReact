7663f3f1d2d6023c41985f293b10fda2e088ff43
Kevinpaulbabu1820 <kevinpaulbabu1820@gmail.com>
2023-08-08 12:29:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import logo from './logo.svg';
+import './App.css';
+import  {useState} from 'react'
+function App() {
+  const [todos,setTodos]=useState([])
+  const [todo,setTodo]=useState('')
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDay = daysOfWeek[new Date().getDay()];
+  return (
+    <div className="app">  
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {currentDay}  ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...todos,{id:Date.now(), text: todo,status:false}])}className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+       { todos.map((obj)=>{
+       return (<div className="todo">
+          <div className="left">
+          <input onChange={(e)=>{
+            setTodos(todos.filter(obj2=>{
+              if (obj2.id===obj.id) {
+                obj2.status=e.target.value
+              }
+              return obj2
+            }))
+          }}
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+              <i onClick={(e)=>{
+                setTodos(todos.filter(obj2 => {
+                  let temp;
+                  if (obj2.id != obj.id){
+                    temp = obj2
+                  }
+                  return temp;
+                }));
+              }} className="fas fa-times"></i>
+          </div>
+          
+        </div>)
+}) } 
+
+
+{todos.map((obj)=>{
+  if (obj.status) {
+    return(<h1>{obj.text}</h1>)
+  }
+  return null
+})}
+ 
+      </div> 
+      <footer>
+        <small>&copy; Copyright {new Date().getFullYear()}, Kevin Paul Babu</small>
+      </footer>  
+    </div>
+
+  );
+}  
+
+export default App;
+ 
